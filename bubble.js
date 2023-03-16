e5d0a6329f5e6f04c175842a5b5026fc7d02703f
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-03-16 12:33:51 -0700

    adding completed sorting algorithm exercises

--- bubble.js
+++ bubble.js
@@ -1,3 +1,18 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let swap = false;
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swap = true;
+      }
+    }
+    if (!swap) break;
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,49 @@
-function merge() {}
+function merge(arr1, arr2) {
+  // Array pointers for comparison
+  let arr1Pointer = 0;
+  let arr2Pointer = 0;
 
-function mergeSort() {}
+  //   Store merged array
+  let mergedArray = [];
 
-module.exports = { merge, mergeSort};
+  //   Iterate till both the arrays aren't exhausted
+  while (arr1Pointer < arr1.length && arr2Pointer < arr2.length) {
+    if (arr1[arr1Pointer] > arr2[arr2Pointer]) {
+      mergedArray.push(arr2[arr2Pointer]);
+      arr2Pointer++;
+    } else {
+      mergedArray.push(arr1[arr1Pointer]);
+      arr1Pointer++;
+    }
+  }
+
+  /*****   Cases where one of the arrays get exhausted *****/
+
+  //    If arr1 still have elements left
+  while (arr1Pointer < arr1.length) {
+    mergedArray.push(arr1[arr1Pointer]);
+    arr1Pointer++;
+  }
+
+  //    If arr2 still have elements left
+  while (arr2Pointer < arr2.length) {
+    mergedArray.push(arr2[arr2Pointer]);
+    arr2Pointer++;
+  }
+
+  return mergedArray;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  // Get midpoint of an array
+  let midPoint = Math.floor(arr.length / 2);
+  //   Store to splitted arrays
+  let left = mergeSort(arr.slice(0, midPoint));
+  let right = mergeSort(arr.slice(midPoint));
+
+  return merge(left, right);
+}
+
+module.exports = { merge, mergeSort };
